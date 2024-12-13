6d7304c625b3daaeae37f1b6f8230996176054cd
Dmitriy Shilin <dshil@fastmail.com>
2024-12-13 09:08:03 +0100

    Update component description

--- components/device/data_handler.go
+++ components/device/data_handler.go
@@ -3,7 +3,7 @@ package device
 // JSON device data.
 type JSON = map[string]any
 
-// DataHandler to handle varios data types from a device.
+// DataHandler handles varios data types from a device.
 type DataHandler interface {
 	// HandleTelemetry handles the telemetry data from the device.
 	HandleTelemetry(deviceID string, js JSON) error
